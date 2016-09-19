ad7f46da189b40abbdb9005250972278ebf88d0d
kefala <martin.g.lobo@gmail.com>
2016-09-18 23:53:57 -0300

    TP 1

---
+++ tp-1.sql
@@ -0,0 +1,35 @@
+create database laboratorio;
+use laboratorio;
+show tables;
+
+CREATE TABLE clientes (
+	numcli INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	nombre VARCHAR(50) NOT NULL,
+	representante INT NOT NULL,
+	limitecredito FLOAT NULL
+);
+
+INSERT INTO laboratio VALUES 
+(1001, "Juan Alberto Paz", 106, 1300.00),
+(1002,"Alvaro Sanz",101,1300.00),
+(1003,"Jorge Lorens",105,1000.00),
+(1004, "Antonio Canales", 101, 900.00),
+(1005,"Juan Suárez",102,1300.00),
+(1007,"Julián López",110,700.00),
+(1008,"Julia Miguez",109,1100.00),
+(1010,"Alberto Romero",103, 500.00),
+(1011,"Carlos García",103,1000.00),
+(1012,"Marta Alvarez",108,1000.00),
+(1013,"Ana Marat",104,400.00),
+(1014,"Cristina Caceres", 102,400.00),
+(1015,"Antonio Guasch",101,400.00),
+(1016,"Oscar Tarragos",106,700.00),
+(1017,"Juan Alvarez",108,1200.00),
+(1019,"Caludia Buena",109,500.00),
+(1020,"Jose Marmol",102,1000.00),
+(1021,"Vicente Tur",103,900.00),
+(1022,"Juan Bottini",105,600.00),
+(1023,"Pedro Soto",102,800.00),
+(1024,"Juan Zapata",107,800.00);
+
+
