db132e0fe89eb139965512a856f3ce148170735e
Martín Lobo <kefala@MacBook-Pro-de-Martin.local>
2016-10-03 16:37:06 -0300

    Tp 2

--- tp-2.sql
+++ tp-2.sql
@@ -12,3 +12,63 @@ SELECT numpedido, producto, cant, importe, (importe*cant) AS total FROM pedidos;
 Calcular cuántos días lleva trabajando cada empleado en la empresa.
 */
 SELECT nombre, TIMESTAMPDIFF(day, contrato, NOW()) AS 'dias en la empresa' FROM empleados;
+
+/*
+Listar los clientes agrupados por representante.
+*/
+SELECT COUNT(nombre) as clientes, representate
+FROM clientes
+GROUP BY representate
+ORDER BY clientes DESC;
+
+/*
+Mostrar las cinco líneas de pedido más caras.
+*/
+SELECT * 
+FROM pedidos 
+ORDER BY importe DESC
+LIMIT 5;
+
+SELECT TOP 5 * 
+FROM pedidos 
+ORDER BY importe DESC;
+/*
+Reformar el para ver únicamente las 5 líneas de pedido de menor precio unitario.
+*/
+SELECT * 
+FROM productos
+ORDER BY precio ASC
+LIMIT 5;
+
+SELECT TOP 5 * 
+FROM productos
+ORDER BY precio ASC;
+/*
+Mostrar toda la información de los pedidos de abril.
+*/
+
+SELECT *
+FROM pedidos
+WHERE DATE_FORMAT(fechapedido, '%c') = 4;
+/*
+Listar los números de empleados que tienen una oficina asignada.
+*/
+
+SELECT count(*) AS empleados, oficina
+FROM empleados
+WHERE oficina IS NOT NULL
+GROUP BY oficina;
+/*
+Listar los empleados de nombre Luis.
+*/
+SELECT *
+FROM empleados
+WHERE nombre LIKE 'Luis%';
+
+/*
+Listar los productos que terminen con a.
+*/
+
+SELECT *
+FROM productos
+WHERE descripcion LIKE '%a';
